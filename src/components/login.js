ce7542be3403f0719eacac188d3b396df791ac47
egrossetto <eugerosso22@hotmail.com>
2020-05-06 14:52:17 -0300

    first cmmit with hooks !

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,7 @@ import { Link, Route } from 'react-router-dom';
 import { withAlert } from "react-alert";
 import signUp from './signUp';
 
-class loginForm extends React.Component {
+/*class loginForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -70,4 +70,55 @@ class loginForm extends React.Component {
     }
   }
 
-  export default withAlert(loginForm);
+  export default withAlert(loginForm);*/
+
+export default Login => {
+    const [loginForm, setLoginForm] = useState({
+        userName: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = (event) => {
+        if (loginForm.userName !== '' && loginForm.password !== '' ) {
+            fetch('http://localhost:3000/api/v1/login', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then(result => result.json())
+            .then( result => {
+                sessionStorage.setItem('token', result.token);
+                sessionStorage.setItem('name', result.name);
+                sessionStorage.setItem('lastName', result.lastName);
+                sessionStorage.setItem('userName', result.userName);
+                sessionStorage.setItem('mail', result.mail);
+                //sessionStorage.setItem('tweets', result.tweets);
+                //sessionStorage.setItem('followers', result.followers);
+                //sessionStorage.setItem('following', result.following);
+                
+                window.location.href = "/";
+            });
+        } else {
+            alert("Por favor ingrese todos los campos.");
+        }
+    }
+
+    return(
+        <div className="login-center">
+            <input onChange={handleChange} placeholder="Username" type="text" name="userName" />
+            <input onChange={handleChange} placeholder="Password"  type="password" name="password" />
+            <Button onClick={handleSubmit}>LOGIN</Button>
+            <br />
+            <Button><Link to='/signup'>SIGN UP</Link></Button>
+            <Route path='/signup' component={signUp}></Route>        
+        </div>
+    )
+}
